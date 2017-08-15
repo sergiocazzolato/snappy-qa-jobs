684680c52bbca6b9bdca367e8ac2a51d3f3af8cf
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-08-15 13:19:10 -0300

    fix on job.yaml

--- scripts/test_flinger/snapd/job_vm.sh
+++ scripts/test_flinger/snapd/job_vm.sh
@@ -19,7 +19,7 @@ test_data:
         - ssh $DEVICE_USER@{device_ip} "sudo apt update && sudo apt install -y git curl"
         - ssh $DEVICE_USER@{device_ip} "git clone $JOBS_URL"        
         - ssh $DEVICE_USER@{device_ip} "git clone $VALIDATOR_URL"
-        - ssh $DEVICE_USER@{device_ip} "git clone $PROJECT_URL"
+        - ssh $DEVICE_USER@{device_ip} "git clone $SNAPD_URL"
         - ssh $DEVICE_USER@{device_ip} "$JOBS_PROJECT/scripts/utils/create_vm.sh $ARCHITECTURE $CHANNEL $PORT"
         - ssh $DEVICE_USER@{device_ip} "(cd $PROJECT && git checkout $BRANCH && cd ..)"
         - ssh $DEVICE_USER@{device_ip} "$JOBS_PROJECT/scripts/utils/run_setup.sh {device_ip} $PORT $TEST_USER \"$SETUP\" "
