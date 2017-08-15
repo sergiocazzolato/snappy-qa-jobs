d9f507f5ba4e4c27344d3d0e213a4311bb6000d6
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-08-15 02:08:18 -0300

    change on how tpr is validated

--- scripts/utils/get_validator.sh
+++ scripts/utils/get_validator.sh
@@ -3,10 +3,10 @@ set -e
 
 echo "Getting validator project and tpr command"
 
-export VALIDATOR_DIR=$WORKSPACE/validator
+export TPR_DIR=$GOPATH/src/$TPR_URL
 export PATH=$GOPATH/bin:$PATH
 
-if [ -d $VALIDATOR_DIR ]; then
+if [ -d $TPR_DIR ]; then
 	echo "Validator already downloaded"
 else
 	mkdir -p $GOPATH
