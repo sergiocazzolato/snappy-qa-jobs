d2f225244da56afd3481655adb2d175ce0d15e29
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-08-15 13:19:10 -0300

    fix on job.yaml

--- scripts/test_flinger/snapd/job_device.sh
+++ scripts/test_flinger/snapd/job_device.sh
@@ -13,7 +13,7 @@ test_data:
   	test_cmds:
 	  	- sudo apt update && sudo apt install -y git curl
 	    - git clone $JOBS_URL
-	    - git clone $PROJECT_URL
+	    - git clone $SNAPD_URL
 	    - (cd $PROJECT && git checkout $BRANCH && cd ..)
 	    - . "$PROJECT/tests/lib/external/prepare-ssh.sh" {device_ip} $DEVICE_PORT $DEVICE_USER
 	    - . "$JOBS_PROJECT/scripts/utils/run_setup.sh" {device_ip} $DEVICE_PORT $TEST_USER $SETUP
