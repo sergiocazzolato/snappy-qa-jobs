7497e4220616796e78e58e21bfa9e7f0a28de98a
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-08-15 00:58:52 -0300

    Creating dependencies file

--- scripts/utils/get_spread.sh
+++ scripts/utils/get_spread.sh
@@ -10,7 +10,6 @@ if [ -f $SPREAD_DIR/spread ]; then
 	echo "Spread already downloaded"
 else
 	mkdir -p $SPREAD_DIR
-	sudo apt install -y curl
 	(cd "$SPREAD_DIR" && curl -s -O https://niemeyer.s3.amazonaws.com/spread-amd64.tar.gz && tar xzvf spread-amd64.tar.gz )	
 fi
 
