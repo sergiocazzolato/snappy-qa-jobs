2714fc87c5245a537fe87bcd28d8b7d88f0225d4
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-08-16 16:13:07 -0300

    Fix for when prepare has been done

--- scripts/external/snapd/run_device.sh
+++ scripts/external/snapd/run_device.sh
@@ -2,7 +2,7 @@
 
 git clone $SNAPD_URL
 (cd $PROJECT && git checkout $BRANCH)
-. "$PROJECT/tests/lib/external/prepare-ssh.sh" "$DEVICE_IP" "$DEVICE_PORT" "$DEVICE_USER"
+. "$PROJECT/tests/lib/external/prepare-ssh.sh" "$DEVICE_IP" "$DEVICE_PORT" "$DEVICE_USER" || true
 . "$SCRIPTS_DIR/utils/run_setup.sh" "$DEVICE_IP" "$DEVICE_PORT" "$TEST_USER" "$SETUP"
 . "$SCRIPTS_DIR/utils/get_spread.sh"
 . "$SCRIPTS_DIR/utils/run_spread.sh" "$DEVICE_IP" "$DEVICE_PORT" "$PROJECT" "$SPREAD_TESTS" "$SPREAD_ENV" | tee run_spread.log
