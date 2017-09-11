11c3ec89dd1ef92c448ee3c2252e10891d247dc0
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-09-11 14:20:27 -0300

    Change how vm is created, reusing snapd nested suite approach

--- scripts/utils/create_vm.sh
+++ scripts/utils/create_vm.sh
@@ -8,8 +8,6 @@ CHANNEL=$2
 PORT=$3
 BRANCH=$4
 
-export WORKSPACE=${WORKSPACE:-$(pwd)}
-
 execute_remote(){
     sshpass -p ubuntu ssh -p $PORT -q -o ConnectTimeout=10 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no user1@localhost "$*"
 }
@@ -32,47 +30,68 @@ prepare_ssh(){
     execute_remote "echo 'test ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/test-user"
 }
 
-# Define variables
-if [ $ARCHITECTURE == "amd64" ]; then
-	ASSERTION=nested-amd64.model
-    QEMU=qemu-system-x86_64
-    PLATFORM=pc-amd64
-    IMG="pc.img"
-elif [ $ARCHITECTURE == "i386" ]; then
-	ASSERTION=nested-i386.model
-    QEMU=qemu-system-i386
-    PLATFORM=pc-i386
-    IMG="pc.img"
-else
-	echo "Architecture $ARCHITECTURE not supported"
-	exit 1
-fi
+create_assertions_disk(){
+    dd if=/dev/null of=assertions.disk bs=1M seek=1
+    mkfs.ext4 -F assertions.disk
+    mkdir /mnt/assertions
+    mount -t ext4 -o loop assertions.disk /mnt/assertions
+    cp "$TESTSLIB/assertions/auto-import.assert" /mnt/assertions
+    umount /mnt/assertions && rm -rf /mnt/assertions
+}
 
-# Install the dependencies
-sudo apt install -y snapd qemu genisoimage sshpass unzip
-sudo snap install ubuntu-image --edge --classic
+systemd_create_and_start_unit() {
+    printf "[Unit]\nDescription=For testing purposes\n[Service]\nType=simple\nExecStart=%s\n" "$2" > "/run/systemd/system/$1.service"
+    if [ -n "${3:-}" ]; then
+        echo "Environment=$3" >> "/run/systemd/system/$1.service"
+    fi
+    systemctl daemon-reload
+    systemctl start "$1"
+}
 
-# Prepare the image under test
-WORKDIR=$WORKSPACE/work-dir
-mkdir -p $WORKDIR
+echo "installing dependencies"
+sudo apt update
+sudo apt install -y snapd qemu genisoimage sshpass
+sudo snap install --classic --beta ubuntu-image
 
-if [ ! -d snapd ]; then 
+echo "Download snapd and checkout branch"
+if [ ! -d snapd ]; then
     git clone https://github.com/snapcore/snapd
 fi
-sudo /snap/bin/ubuntu-image --image-size 3G snapd/tests/lib/assertions/$ASSERTION --channel $CHANNEL -O $WORKDIR
-genisoimage -volid cidata -joliet -rock -o assertions.disk snapd/tests/lib/assertions/auto-import.assert
-mv assertions.disk $WORKDIR
-
-if [ ! -f $WORKDIR/$IMG ] || [ ! -f $WORKDIR/assertions.disk ]; then
-    echo "Some needed files does not exist, could be either $WORKDIR/$IMG or $WORKDIR/assertions.disk"
-    exit 1
+if [ ! -z $BRANCH ]; then
+    (cd snapd && git checkout $BRANCH)
 fi
+export TESTSLIB="./snapd/tests/lib"
 
-# Run the vm
-sudo systemd-run --unit sut-vm /usr/bin/$QEMU -m 1024 -nographic -net nic,model=virtio -net user,hostfwd=tcp::$PORT-:22 -drive file=$WORKDIR/$IMG,if=virtio,cache=none -drive file=$WORKDIR/assertions.disk,if=virtio,cache=none -machine accel=kvm
+# determine arch related vars
+case "$ARCHITECTURE" in
+amd64)
+    QEMU="$(which qemu-system-x86_64)"
+    ;;
+i386)
+    QEMU="$(which qemu-system-i386)"
+    ;;
+*)
+    echo "unsupported architecture"
+    exit 1
+    ;;
+esac
 
-echo "Wait for ssh ready and addind test user to the vm"
+# create ubuntu-core image
+mkdir -p /tmp/work-dir
+
+snap download core
+
+/snap/bin/ubuntu-image --image-size 3G "$TESTSLIB/assertions/nested-${ARCHITECTURE}.model" --channel "$CHANNEL" --output ubuntu-core.img --extra-snaps core_*.snap
+mv ubuntu-core.img /tmp/work-dir
+
+create_assertions_disk
+
+systemd_create_and_start_unit nested-vm "${QEMU} -m 1024 -nographic -net nic,model=virtio -net user,hostfwd=tcp::$PORT-:22 -drive file=/tmp/work-dir/ubuntu-core.img,if=virtio,cache=none -drive file=${PWD}/assertions.disk,if=virtio,cache=none -machine accel=kvm"
+
 wait_for_ssh
 prepare_ssh
+
+echo "Wait for first boot to be done"
+while ! execute_remote "snap changes" | MATCH "Done.*Initialize system state"; do sleep 1; done
 
 echo "VM Ready"
