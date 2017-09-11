8ea5b624ea774f7f0d8e4de8137b7b649e7eb564
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-09-11 19:01:54 -0300

    Creating vm as sudo for cconf tests

--- scripts/test_flinger/console-conf-tests/job_vm.sh
+++ scripts/test_flinger/console-conf-tests/job_vm.sh
@@ -17,7 +17,7 @@ test_data:
         - ssh $DEVICE_USER@{device_ip} "git clone $VALIDATOR_URL"
         - ssh $DEVICE_USER@{device_ip} "git clone $CCONF_URL"
         - ssh $DEVICE_USER@{device_ip} "(cd $PROJECT && git checkout $BRANCH)"
-        - ssh $DEVICE_USER@{device_ip} ". $JOBS_PROJECT/scripts/utils/create_vm.sh \"$ARCHITECTURE\" \"$CHANNEL\" \"$PORT\""
+        - ssh $DEVICE_USER@{device_ip} "sudo $JOBS_PROJECT/scripts/utils/create_vm.sh \"$ARCHITECTURE\" \"$CHANNEL\" \"$PORT\""
         - ssh $DEVICE_USER@{device_ip} ". $JOBS_PROJECT/scripts/utils/run_setup.sh \"$HOST\" \"$PORT\" \"$TEST_USER\" \"\" \"$SETUP\""
         - ssh $DEVICE_USER@{device_ip} ". $JOBS_PROJECT/scripts/utils/get_spread.sh"
         - ssh $DEVICE_USER@{device_ip} ". $JOBS_PROJECT/scripts/utils/run_spread.sh \"$HOST\" \"$PORT\" \"$PROJECT\" \"$SPREAD_TESTS\" \"$SPREAD_ENV\""
