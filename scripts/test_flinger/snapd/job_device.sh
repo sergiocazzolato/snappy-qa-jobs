16f31a8baf1b2aec166af3f541691a45d5d1ebab
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-09-15 17:35:17 -0300

    adding text blocks instead of lists to tf jobs

--- scripts/test_flinger/snapd/job_device.sh
+++ scripts/test_flinger/snapd/job_device.sh
@@ -7,16 +7,17 @@ job_queue: $DEVICE_QUEUE
 provision_data:
     channel: $CHANNEL
 test_data:
-    test_cmds:
-        - sudo apt update && sudo apt install -y git curl
-        - git clone $JOBS_URL
-        - (cd $JOBS_PROJECT && git checkout $JOBS_BRANCH)
-        - git clone $SNAPD_URL
-        - (cd $PROJECT && git checkout $BRANCH)
-        - . $PROJECT/tests/lib/external/prepare-ssh.sh "{device_ip}" "$DEVICE_PORT" "$DEVICE_USER"
-        - . $JOBS_PROJECT/scripts/utils/run_setup.sh "{device_ip}" "$DEVICE_PORT" "$TEST_USER" "$TEST_PASS" "$SETUP"
-        - . $JOBS_PROJECT/scripts/utils/get_spread.sh
-        - . $JOBS_PROJECT/scripts/utils/run_spread.sh "{device_ip}" "$DEVICE_PORT" "$PROJECT" "$SPREAD_TESTS" "$SPREAD_ENV" "$SKIP_TESTS"
+    test_cmds: |
+        sudo rm -f /var/lib/apt/lists/lock
+        sudo apt update && sudo apt install -y git curl
+        git clone $JOBS_URL
+        (cd $JOBS_PROJECT && git checkout $JOBS_BRANCH)
+        git clone $SNAPD_URL
+        (cd $PROJECT && git checkout $BRANCH)
+        . $PROJECT/tests/lib/external/prepare-ssh.sh "{device_ip}" "$DEVICE_PORT" "$DEVICE_USER"
+        . $JOBS_PROJECT/scripts/utils/run_setup.sh "{device_ip}" "$DEVICE_PORT" "$TEST_USER" "$TEST_PASS" "$SETUP"
+        . $JOBS_PROJECT/scripts/utils/get_spread.sh
+        . $JOBS_PROJECT/scripts/utils/run_spread.sh "{device_ip}" "$DEVICE_PORT" "$PROJECT" "$SPREAD_TESTS" "$SPREAD_ENV" "$SKIP_TESTS"
 EOF
 
 export TF_JOB=$TF_DATA/job.yaml
--- scripts/test_flinger/snapd/job_vm.sh
+++ scripts/test_flinger/snapd/job_vm.sh
@@ -10,17 +10,18 @@ job_queue: $DEVICE_QUEUE
 provision_data:
     distro: xenial
 test_data:
-    test_cmds:
-        - ssh $DEVICE_USER@{device_ip} "sudo apt update && sudo apt install -y git curl jq"
-        - ssh $DEVICE_USER@{device_ip} "git clone $JOBS_URL"
-        - ssh $DEVICE_USER@{device_ip} "(cd $JOBS_PROJECT && git checkout $JOBS_BRANCH)"
-        - ssh $DEVICE_USER@{device_ip} "git clone $VALIDATOR_URL"
-        - ssh $DEVICE_USER@{device_ip} "git clone $SNAPD_URL"
-        - ssh $DEVICE_USER@{device_ip} "(cd $PROJECT && git checkout $BRANCH)"
-        - ssh $DEVICE_USER@{device_ip} "sudo $JOBS_PROJECT/scripts/utils/create_vm.sh \"$ARCHITECTURE\" \"$CHANNEL\" \"$PORT\""
-        - ssh $DEVICE_USER@{device_ip} ". $JOBS_PROJECT/scripts/utils/run_setup.sh \"$HOST\" \"$PORT\" \"$TEST_USER\" \"$TEST_PASS\" \"$SETUP\""
-        - ssh $DEVICE_USER@{device_ip} ". $JOBS_PROJECT/scripts/utils/get_spread.sh"
-        - ssh $DEVICE_USER@{device_ip} ". $JOBS_PROJECT/scripts/utils/run_spread.sh \"$HOST\" \"$PORT\" \"$PROJECT\" \"$SPREAD_TESTS\" \"$SPREAD_ENV\" \"$SKIP_TESTS\""
+    test_cmds: |
+        ssh $DEVICE_USER@{device_ip} "sudo rm -f /var/lib/apt/lists/lock"
+        ssh $DEVICE_USER@{device_ip} "sudo apt update && sudo apt install -y git curl jq"
+        ssh $DEVICE_USER@{device_ip} "git clone $JOBS_URL"
+        ssh $DEVICE_USER@{device_ip} "(cd $JOBS_PROJECT && git checkout $JOBS_BRANCH)"
+        ssh $DEVICE_USER@{device_ip} "git clone $VALIDATOR_URL"
+        ssh $DEVICE_USER@{device_ip} "git clone $SNAPD_URL"
+        ssh $DEVICE_USER@{device_ip} "(cd $PROJECT && git checkout $BRANCH)"
+        ssh $DEVICE_USER@{device_ip} "sudo $JOBS_PROJECT/scripts/utils/create_vm.sh \"$ARCHITECTURE\" \"$CHANNEL\" \"$PORT\""
+        ssh $DEVICE_USER@{device_ip} ". $JOBS_PROJECT/scripts/utils/run_setup.sh \"$HOST\" \"$PORT\" \"$TEST_USER\" \"$TEST_PASS\" \"$SETUP\""
+        ssh $DEVICE_USER@{device_ip} ". $JOBS_PROJECT/scripts/utils/get_spread.sh"
+        ssh $DEVICE_USER@{device_ip} ". $JOBS_PROJECT/scripts/utils/run_spread.sh \"$HOST\" \"$PORT\" \"$PROJECT\" \"$SPREAD_TESTS\" \"$SPREAD_ENV\" \"$SKIP_TESTS\""
 EOF
 
 export TF_JOB=$TF_DATA/job.yaml
