c7389bda2ec90fdff8815ec48c976188b66b9298
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-09-19 20:34:08 -0300

    fix on how user is registered on device

--- scripts/test_flinger/snapd/job_device.sh
+++ scripts/test_flinger/snapd/job_device.sh
@@ -18,7 +18,7 @@ test_data:
         git clone $SNAPD_URL
         (cd $PROJECT && git checkout $BRANCH)
         . $PROJECT/tests/lib/external/prepare-ssh.sh "{device_ip}" "$DEVICE_PORT" "$DEVICE_USER"
-        . $JOBS_PROJECT/scripts/utils/register_device.sh "{device_ip}" "$DEVICE_PORT" "$DEVICE_USER" "$DEVICE_PASS" "$REGISTER_EMAIL"
+        . $JOBS_PROJECT/scripts/utils/register_device.sh "{device_ip}" "$DEVICE_PORT" "$TEST_USER" "$TEST_PASS" "$REGISTER_EMAIL"
         . $JOBS_PROJECT/scripts/utils/run_setup.sh "{device_ip}" "$DEVICE_PORT" "$TEST_USER" "$TEST_PASS" "$SETUP"
         . $JOBS_PROJECT/scripts/utils/get_spread.sh
         . $JOBS_PROJECT/scripts/utils/run_spread.sh "{device_ip}" "$DEVICE_PORT" "$PROJECT" "$SPREAD_TESTS" "$SPREAD_ENV" "$SKIP_TESTS"
