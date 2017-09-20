f0843cbee61110d0201c5df1543c8565dc756b57
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-09-20 01:49:06 -0300

    adding missing var for db config

--- scripts/env/tf_snapd_db.sh
+++ scripts/env/tf_snapd_db.sh
@@ -6,5 +6,5 @@ export PROJECT=${PROJECT:-"snapd"}
 export CHANNEL=${CHANNEL:-"beta"}
 export DEVICE_QUEUE=${DEVICE_QUEUE:-"dragonboard"}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-arm-64:tests/"}
+export TEST_PASS=${TEST_PASS:-"ubuntu"}
 export SKIP_TESTS=${SKIP_TESTS:-""}
-
