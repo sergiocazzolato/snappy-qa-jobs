1f6f39f3244dc01aa41b124f7f6082840be5f15c
Sergio <sergiocazzolato@gmail.com>
2017-09-25 13:50:42 -0400

    Add generic queue for vms on tf

--- scripts/env/tf_cconf_amd64.sh
+++ scripts/env/tf_cconf_amd64.sh
@@ -6,6 +6,6 @@ export PROJECT=${PROJECT:-"console-conf-tests"}
 export CHANNEL=${CHANNEL:-"beta"}
 export CORE_CHANNEL=${CORE_CHANNEL:-"stable"}
 export ARCHITECTURE=${ARCHITECTURE:-"amd64"}
-export DEVICE_QUEUE=${DEVICE_QUEUE:-"intel-gfx"}
+export DEVICE_QUEUE=${DEVICE_QUEUE:-"maas-x86-node"}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-64"}
 export TEST_PASS=${TEST_PASS:-"ubuntu"}
--- scripts/env/tf_cconf_i386.sh
+++ scripts/env/tf_cconf_i386.sh
@@ -6,6 +6,6 @@ export PROJECT=${PROJECT:-"console-conf-tests"}
 export CHANNEL=${CHANNEL:-"beta"}
 export CORE_CHANNEL=${CORE_CHANNEL:-"stable"}
 export ARCHITECTURE=${ARCHITECTURE:-"i386"}
-export DEVICE_QUEUE=${DEVICE_QUEUE:-"amd-gfx"}
+export DEVICE_QUEUE=${DEVICE_QUEUE:-"maas-x86-node"}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-32"}
 export TEST_PASS=${TEST_PASS:-"ubuntu"}
