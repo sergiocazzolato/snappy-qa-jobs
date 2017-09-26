cd7c9ad5f93bd6d409a7f9e77a4f796c35ebc2c4
sergio-j-cazzolato <sergio.cazzolato@canonical.com>
2017-09-26 08:41:24 -0400

    Adding new config for refresh db

---
+++ scripts/env/tf_snapd_db_refresh.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+. "$SCRIPTS_DIR/env/common.sh"
+
+export PROJECT=${PROJECT:-"snapd"}
+export CHANNEL=${CHANNEL:-"stable"}
+export DEVICE_QUEUE=${DEVICE_QUEUE:-"dragonboard"}
+export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-arm-64:tests/"}
+export TEST_PASS=${TEST_PASS:-"ubuntu"}
+export SKIP_TESTS=${SKIP_TESTS:-""}
+export SETUP=${SETUP:-"sudo snap refresh --beta core"}
