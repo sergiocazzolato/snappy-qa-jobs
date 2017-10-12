deb70fdaf783a907cf13c12c090843d350beb2a5
sergio-j-cazzolato <sergio.cazzolato@canonical.com>
2017-10-11 22:48:31 -0300

    update cc config for pi3

--- scripts/env/dev_cconf_pi3.sh
+++ scripts/env/dev_cconf_pi3.sh
@@ -5,6 +5,7 @@
 export PROJECT=${PROJECT:-"console-conf-tests"}
 export CHANNEL=${CHANNEL:-"beta"}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-arm-32"}
+export WIFI_SSID=${WIFI_READY:-true}
 export WIFI_SSID=${WIFI_SSID:-""}
 export WIFI_PASSWORD=${WIFI_PASSWORD:-""}
 export DEVICE_IP=${DEVICE_IP:-"127.0.0.1"}
