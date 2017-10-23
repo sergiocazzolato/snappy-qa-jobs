f131e8e1177a076ad5051b34acafba021982466d
sergio-j-cazzolato <sergio.cazzolato@canonical.com>
2017-10-23 12:01:26 -0300

    Fix core channel for cconf

--- scripts/env/tf_cconf_amd64.sh
+++ scripts/env/tf_cconf_amd64.sh
@@ -4,7 +4,6 @@
 
 export PROJECT=${PROJECT:-"console-conf-tests"}
 export CHANNEL=${CHANNEL:-"beta"}
-export CORE_CHANNEL=${CORE_CHANNEL:-"stable"}
 export ARCHITECTURE=${ARCHITECTURE:-"amd64"}
 export DEVICE_QUEUE=${DEVICE_QUEUE:-"maas-x86-node"}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-64"}
