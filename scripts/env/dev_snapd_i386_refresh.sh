beda50d471ce285f5ced39a861c72ce28680fd9e
sergio-j-cazzolato <sergio.cazzolato@canonical.com>
2017-10-28 16:18:43 -0300

    Clean up the /home/gopath before run snapd tests

--- scripts/env/dev_snapd_i386_refresh.sh
+++ scripts/env/dev_snapd_i386_refresh.sh
@@ -5,6 +5,6 @@
 export PROJECT=${PROJECT:-"snapd"}
 export CHANNEL=${CHANNEL:-"stable"}
 export CORE_CHANNEL=${CORE_CHANNEL:-"beta"}
-export SETUP=${SETUP:-""}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-32"}
 export DEVICE_IP=${DEVICE_IP:-"127.0.0.1"}
+export SETUP=${SETUP:-"sudo rm -rf /home/gopath"}
--- scripts/env/dev_snapd_pi.sh
+++ scripts/env/dev_snapd_pi.sh
@@ -6,3 +6,4 @@ export PROJECT=${PROJECT:-"snapd"}
 export CHANNEL=${CHANNEL:-"beta"}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-arm-32"}
 export DEVICE_IP=${DEVICE_IP:-"127.0.0.1"}
+export SETUP=${SETUP:-"sudo rm -rf /home/gopath"}
