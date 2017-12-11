e40ed0fbe3834cb59340f0faa4122c4cccdcdd75
sergio-j-cazzolato <sergio.cazzolato@canonical.com>
2017-12-11 18:45:09 -0300

    Add support for ubuntu-17.10-64 on sru execution

--- scripts/linode/snapd/run_linode_sru.sh
+++ scripts/linode/snapd/run_linode_sru.sh
@@ -5,5 +5,6 @@ git clone $SNAPD_URL $PROJECT
 . "$SCRIPTS_DIR/utils/get_spread.sh"
 cp $PROJECT/spread.yaml $PROJECT/spread.yaml.bak
 sed 's/ubuntu-16.04-32/ubuntu-17.04-64/g' -i $PROJECT/spread.yaml
+sed 's/debian-9-64/ubuntu-17.10-64/g' -i $PROJECT/spread.yaml
 . "$SCRIPTS_DIR/utils/run_spread.sh" "127.0.0.1" "22" "$PROJECT" "$SPREAD_TESTS" "$SPREAD_ENV" "$SKIP_TESTS" | tee run_spread.log
 cp $PROJECT/spread.yaml.bak $PROJECT/spread.yaml
