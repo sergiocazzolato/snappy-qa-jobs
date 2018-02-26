42b07eb3c41c9e9ec327e04ce2c8835670654234
sergio-j-cazzolato <sergio.cazzolato@canonical.com>
2018-02-26 17:53:38 -0300

    Update to run sru validation in linode

--- scripts/env/lin_snapd_sru_validation.sh
+++ scripts/env/lin_snapd_sru_validation.sh
@@ -4,6 +4,6 @@
 
 export PROJECT=${PROJECT:-"snapd"}
 export CHANNEL=${CHANNEL:-"beta"}
-export SPREAD_TESTS=${SPREAD_TESTS:-"linode:ubuntu-14.04-64 linode:ubuntu-16.04-64 linode:ubuntu-17.04-64 linode:ubuntu-17.10-64"}
+export SPREAD_TESTS=${SPREAD_TESTS:-"linode-sru"}
 export SPREAD_ENV=${SPREAD_ENV:-"SPREAD_MODIFY_CORE_SNAP_FOR_REEXEC=0 SPREAD_TRUST_TEST_KEYS=false SPREAD_SNAP_REEXEC=0 SPREAD_CORE_CHANNEL=beta"}
 export SPREAD_PARAMS=${SPREAD_PARAMS:-"-v"}
