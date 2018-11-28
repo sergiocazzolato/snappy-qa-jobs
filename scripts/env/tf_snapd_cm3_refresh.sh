5779770d9f89d76b7ce1ff539f8d6bb352c2fa3a
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2018-11-28 13:57:54 -0300

    update readme and config

--- scripts/env/tf_snapd_cm3_refresh.sh
+++ scripts/env/tf_snapd_cm3_refresh.sh
@@ -3,7 +3,6 @@
 . "$SCRIPTS_DIR/env/common.sh"
 
 export PROJECT=${PROJECT:-"snapd"}
-export IMAGE_URL=${IMAGE_URL:-"http://cdimage.ubuntu.com/ubuntu-core/16/stable/current/ubuntu-core-16-cm3.img.xz"}
 export DEVICE_QUEUE=${DEVICE_QUEUE:-"cm3"}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-arm-32:tests/"}
 export SPREAD_PARAMS=${SPREAD_PARAMS:-"-v"}
