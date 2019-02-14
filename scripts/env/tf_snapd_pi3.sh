a02d249ee3996eb6ce5dc3bdfec34f013e917517
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2019-02-14 11:00:15 -0300

    updating device queue to rpi3b

--- scripts/env/tf_snapd_pi3.sh
+++ scripts/env/tf_snapd_pi3.sh
@@ -4,7 +4,7 @@
 
 export PROJECT=${PROJECT:-"snapd"}
 export CHANNEL=${CHANNEL:-"beta"}
-export DEVICE_QUEUE=${DEVICE_QUEUE:-"pi3b"}
+export DEVICE_QUEUE=${DEVICE_QUEUE:-"rpi3b"}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-arm-32:tests/"}
 export SPREAD_PARAMS=${SPREAD_PARAMS:-"-v"}
 export TEST_PASS=${TEST_PASS:-"ubuntu"}
--- scripts/env/tf_snapd_pi3_refresh.sh
+++ scripts/env/tf_snapd_pi3_refresh.sh
@@ -3,7 +3,7 @@
 . "$SCRIPTS_DIR/env/common.sh"
 
 export PROJECT=${PROJECT:-"snapd"}
-export DEVICE_QUEUE=${DEVICE_QUEUE:-"pi3b"}
+export DEVICE_QUEUE=${DEVICE_QUEUE:-"rpi3b"}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-arm-32:tests/"}
 export SPREAD_PARAMS=${SPREAD_PARAMS:-"-v"}
 export TEST_PASS=${TEST_PASS:-"ubuntu"}
