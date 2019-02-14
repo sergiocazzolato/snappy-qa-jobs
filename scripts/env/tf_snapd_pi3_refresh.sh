e4f22b9a41a35f7bbe9e1989973f257872096f69
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2019-02-14 10:16:05 -0300

    Adding config files for pi3 on test flinger

---
+++ scripts/env/tf_snapd_pi3_refresh.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+. "$SCRIPTS_DIR/env/common.sh"
+
+export PROJECT=${PROJECT:-"snapd"}
+export DEVICE_QUEUE=${DEVICE_QUEUE:-"pi3b"}
+export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-arm-32:tests/"}
+export SPREAD_PARAMS=${SPREAD_PARAMS:-"-v"}
+export TEST_PASS=${TEST_PASS:-"ubuntu"}
+export SKIP_TESTS=${SKIP_TESTS:-"tests/main/auto-refresh,tests/main/interfaces-content,tests/main/install-sideload,tests/main/snap-core-symlinks"}
+export CORE_CHANNEL=${CORE_CHANNEL:-"beta"}
+export SETUP=${SETUP:-""}
