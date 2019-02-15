c33f99a773a64ac02f7550f2d761558c1c912daf
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2019-02-15 10:06:29 -0300

    New env files configured for core18

---
+++ scripts/env/tf_snapd_db_18.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+. "$SCRIPTS_DIR/env/common.sh"
+
+export PROJECT=${PROJECT:-"snapd"}
+export CHANNEL=${CHANNEL:-"beta"}
+export DEVICE_QUEUE=${DEVICE_QUEUE:-"dragonboard"}
+export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-18-arm-64:tests/"}
+export SPREAD_PARAMS=${SPREAD_PARAMS:-"-v"}
+export TEST_PASS=${TEST_PASS:-"ubuntu"}
+export SKIP_TESTS=${SKIP_TESTS:-"tests/main/auto-refresh"}
--- scripts/env/tf_snapd_pi3_refresh_18.sh
+++ scripts/env/tf_snapd_pi3_refresh_18.sh
@@ -3,11 +3,10 @@
 . "$SCRIPTS_DIR/env/common.sh"
 
 export PROJECT=${PROJECT:-"snapd"}
-export CHANNEL=${CHANNEL:-"stable"}
-export CORE_CHANNEL=${CORE_CHANNEL:-"beta"}
-export DEVICE_QUEUE=${DEVICE_QUEUE:-"dragonboard"}
-export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-arm-64:tests/"}
+export DEVICE_QUEUE=${DEVICE_QUEUE:-"rpi3b"}
+export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-18-arm-32:tests/"}
 export SPREAD_PARAMS=${SPREAD_PARAMS:-"-v"}
 export TEST_PASS=${TEST_PASS:-"ubuntu"}
 export SKIP_TESTS=${SKIP_TESTS:-"tests/main/auto-refresh,tests/main/interfaces-content,tests/main/install-sideload,tests/main/snap-core-symlinks"}
+export CORE_CHANNEL=${CORE_CHANNEL:-"beta"}
 export SETUP=${SETUP:-""}
