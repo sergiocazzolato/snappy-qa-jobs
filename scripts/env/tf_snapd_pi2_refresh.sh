7a69d4b2da8768537029adb019175b3fa31cc1cf
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2019-03-15 10:29:26 -0300

    Add support for rpi2 ontest flinger

---
+++ scripts/env/tf_snapd_pi2_refresh.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+. "$SCRIPTS_DIR/env/common.sh"
+
+export ARCH=${ARCH:-"armhf"}
+export PROJECT=${PROJECT:-"snapd"}
+export DEVICE_QUEUE=${DEVICE_QUEUE:-"rpi2"}
+export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-arm-32:tests/"}
+export SPREAD_PARAMS=${SPREAD_PARAMS:-"-v"}
+export TEST_PASS=${TEST_PASS:-"ubuntu"}
+export SKIP_TESTS=${SKIP_TESTS:-"tests/main/auto-refresh,tests/main/interfaces-content,tests/main/install-sideload,tests/main/snap-core-symlinks"}
+export CORE_CHANNEL=${CORE_CHANNEL:-"beta"}
+export SETUP=${SETUP:-""}
