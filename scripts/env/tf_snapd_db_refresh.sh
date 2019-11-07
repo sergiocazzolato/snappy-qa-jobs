33f6ab245ab9e1288439493ca36512a6ff565121
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2019-11-07 11:48:05 -0300

    Skiping interfaces-many-*-provided test on amd64 and dragonboard
    This is because it goes out of memory

--- scripts/env/tf_snapd_db_refresh.sh
+++ scripts/env/tf_snapd_db_refresh.sh
@@ -11,5 +11,4 @@ export DEVICE_QUEUE=${DEVICE_QUEUE:-"dragonboard"}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-arm-64:tests/"}
 export SPREAD_PARAMS=${SPREAD_PARAMS:-"-v"}
 export TEST_PASS=${TEST_PASS:-"ubuntu"}
-export SKIP_TESTS=${SKIP_TESTS:-"tests/main/interfaces-content,tests/main/install-sideload"}
-export SETUP=${SETUP:-""}
+export SKIP_TESTS=${SKIP_TESTS:-"tests/main/interfaces-many-snap-provided,tests/main/interfaces-many-core-provided"}
