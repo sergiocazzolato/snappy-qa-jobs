d41c5ac680021d8a1385a216733b0932a9f005b9
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2019-11-20 17:45:39 -0300

    Changes which were not uploaded on last push

--- services/env/snapd-beta-full-i386-refresh-18.sh
+++ services/env/snapd-beta-full-i386-refresh-18.sh
@@ -4,4 +4,4 @@ export SNAP_UT=snapd
 export ARCH_UT=i386
 export BOARD_UT=pc-i386-refresh-18
 export TARGET_CHANNEL=beta
-export TRELLO_CONFIG=trello_card_snapd_18.yaml
+export TRELLO_CONFIG=trello_card_snapd_18_beta.yaml
--- services/env/snapd-beta-full-pi2-18.sh
+++ services/env/snapd-beta-full-pi2-18.sh
@@ -4,4 +4,4 @@ export SNAP_UT=snapd
 export ARCH_UT=arm32
 export BOARD_UT=pi2-18
 export TARGET_CHANNEL=beta
-export TRELLO_CONFIG=trello_card_snapd_18.yaml
+export TRELLO_CONFIG=trello_card_snapd_18_beta.yaml
