69deb7f74a8a96833f997a920145c243bf37a404
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2020-02-03 19:03:52 -0300

    skipping wifi tests on db for consoleconf because possible security leak

--- scripts/env/tf_cconf_db.sh
+++ scripts/env/tf_cconf_db.sh
@@ -8,6 +8,6 @@ export CHANNEL=${CHANNEL:-"beta"}
 export DEVICE_QUEUE=${DEVICE_QUEUE:-"dragonboard"}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-arm-64"}
 export SPREAD_PARAMS=${SPREAD_PARAMS:-"-v"}
-export SPREAD_ENV=${SPREAD_ENV:-"WIFI_READY=true"}
+export SPREAD_ENV=${SPREAD_ENV:-"WIFI_READY=false"}
 export SKIP_REFRESH=${SKIP_REFRESH:-"true"}
 export TEST_PASS=${TEST_PASS:-"ubuntu"}
