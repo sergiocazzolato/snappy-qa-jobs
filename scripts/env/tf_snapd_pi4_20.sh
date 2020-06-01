3a90e73c66639110be1a36252081f648c3f68301
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2020-06-01 17:23:08 -0300

    Adding scripts for pi4 on uc20

--- scripts/env/tf_snapd_pi4_20.sh
+++ scripts/env/tf_snapd_pi4_20.sh
@@ -4,12 +4,9 @@
 
 export ARCH=${ARCH:-"arm64"}
 export PROJECT=${PROJECT:-"snapd"}
-export CHANNEL=${CHANNEL:-"stable"}
-export CORE_CHANNEL=${CORE_CHANNEL:-"beta"}
-export SNAPD_CHANNEL=${SNAPD_CHANNEL:-"beta"}
-export DEVICE_QUEUE=${DEVICE_QUEUE:-"dragonboard"}
+export CHANNEL=${CHANNEL:-"beta"}
+export DEVICE_QUEUE=${DEVICE_QUEUE:-"rpi4b4g"}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-20-arm-64:tests/"}
 export SPREAD_PARAMS=${SPREAD_PARAMS:-"-v"}
 export TEST_PASS=${TEST_PASS:-"ubuntu"}
 export SKIP_TESTS=${SKIP_TESTS:-""}
-export SETUP=${SETUP:-""}
--- services/env/snapd-beta-full-pi4-refresh-20.sh
+++ services/env/snapd-beta-full-pi4-refresh-20.sh
@@ -2,6 +2,6 @@
 
 export SNAP_UT=snapd
 export ARCH_UT=arm64
-export BOARD_UT=dragonboard-20
+export BOARD_UT=pi4-refresh-20
 export TARGET_CHANNEL=beta
 export TRELLO_CONFIG=trello_card_snapd_20_beta.yaml
