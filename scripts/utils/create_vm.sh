073c5932aebecaa1fdc2eaa4f2126a6ee023a079
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2020-06-01 18:13:25 -0300

    Update the script to create vm on uc20

--- scripts/utils/create_vm.sh
+++ scripts/utils/create_vm.sh
@@ -22,95 +22,49 @@ IMAGE_URL=$2
 USER_ASSERTION_URL=$3
 BUILD_SNAPD=$4
 
-if test "$(lsb_release -cs)" = focal; then
-    # Install snapd and add variables
-    mkdir -p /home/gopath
-    wget https://github.com/snapcore/snapd/archive/master.zip
-    unzip -q master.zip
 
-    export PROJECT_PATH="./snapd-master"
-    export TESTSLIB=./snapd-master/tests/lib
-    export GOHOME=/home/gopath
-    export GOPATH="$GOHOME"
-    export SPREAD_PATH="$GOHOME"
-    export SPREAD_SYSTEM=ubuntu-20.04-64
-    export PATH="$GOHOME/bin:/snap/bin:$PATH:/usr/lib/go-1.6/bin:/var/lib/snapd/snap/bin:$TESTSLIB/bin"
+execute_remote(){
+    sshpass -p ubuntu ssh -p $PORT -q -o ConnectTimeout=10 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no user1@localhost "$*"
+}
 
-    # Define variables used to the nested vm
-    export NESTED_TYPE=core
-    export SPREAD_BACKEND=external
-    export NESTED_ARCHITECTURE="$ARCHITECTURE"
-    export CORE_CHANNEL=edge
-    export BUILD_FROM_CURRENT="$BUILD_SNAPD"
-    export USE_CLOUD_INIT=true
-    export ENABLE_SECURE_BOOT=true
-    export ENABLE_TPM=true
-    export CUSTOM_IMAGE_URL="$IMAGE_URL"
-  
-    if [ "$BUILD_FROM_CURRENT" == "true" ]; then
-         # Create test user
-        sudo groupadd --gid 12345 test
-        adduser --uid 12345 --gid 12345 --disabled-password --gecos '' test
+wait_for_ssh(){
+    retry=150
+    while ! execute_remote true; do
+        retry=$(( retry - 1 ))
+        if [ $retry -le 0 ]; then
+            echo "Timed out waiting for ssh. Aborting!"
+            return 1
+        fi
+        sleep 1
+    done
+    return 0
+}
 
-        # Build snapd
-        echo "installing snapd build dependencies"
-        sudo apt install -y golang
-        go get -u github.com/kardianos/govendor
+wait_for_no_ssh(){
+    retry=150
+    while execute_remote true; do
+        retry=$(( retry - 1 ))
+        if [ $retry -le 0 ]; then
+            echo "Timed out waiting for no ssh. Aborting!"
+            return 1
+        fi
+        sleep 1
+    done
+    return 0
+}
 
-        "$TESTSLIB"/prepare-restore.sh --prepare-project
-    fi
+prepare_ssh(){
+    execute_remote "sudo adduser --uid 12345 --extrausers --quiet --disabled-password --gecos '' test"
+    execute_remote "echo test:ubuntu | sudo chpasswd"
+    execute_remote "echo 'test ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/create-user-test"
 
-    # Update the memory used by the vm until is it possible to configure it
-    sed -i -e 's/-m 4096/-m 2048/g' "$TESTSLIB"/nested.sh
+    execute_remote "sudo adduser --extrausers --quiet --disabled-password --gecos '' external"
+    execute_remote "echo external:ubuntu | sudo chpasswd"
+    execute_remote "echo 'external ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/create-user-external"
+}
 
-    # Create and run nested vm
-    . "$TESTSLIB"/nested.sh
-    create_nested_core_vm
-    start_nested_core_vm
-    echo "VM Ready"
-else
-    execute_remote(){
-        sshpass -p ubuntu ssh -p $PORT -q -o ConnectTimeout=10 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no user1@localhost "$*"
-    }
-
-    wait_for_ssh(){
-        retry=150
-        while ! execute_remote true; do
-            retry=$(( retry - 1 ))
-            if [ $retry -le 0 ]; then
-                echo "Timed out waiting for ssh. Aborting!"
-                return 1
-            fi
-            sleep 1
-        done
-        return 0
-    }
-
-    wait_for_no_ssh(){
-        retry=150
-        while execute_remote true; do
-            retry=$(( retry - 1 ))
-            if [ $retry -le 0 ]; then
-                echo "Timed out waiting for no ssh. Aborting!"
-                return 1
-            fi
-            sleep 1
-        done
-        return 0
-    }
-
-    prepare_ssh(){
-        execute_remote "sudo adduser --uid 12345 --extrausers --quiet --disabled-password --gecos '' test"
-        execute_remote "echo test:ubuntu | sudo chpasswd"
-        execute_remote "echo 'test ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/create-user-test"
-
-        execute_remote "sudo adduser --extrausers --quiet --disabled-password --gecos '' external"
-        execute_remote "echo external:ubuntu | sudo chpasswd"
-        execute_remote "echo 'external ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/create-user-external"
-    }
-
-    create_cloud_init_config(){
-        cat <<EOF > "$WORK_DIR/user-data"
+create_cloud_init_config(){
+    cat <<EOF > "$WORK_DIR/user-data"
 #cloud-config
   ssh_pwauth: True
   users:
@@ -123,87 +77,124 @@ else
    expire: False
 EOF
 
-        cat <<EOF > "$WORK_DIR/meta-data"
+    cat <<EOF > "$WORK_DIR/meta-data"
 instance_id: cloud-images
 EOF
 
-        loops=$(kpartx -avs "$WORK_DIR/ubuntu-core.img"  | cut -d' ' -f 3)
-        part=$(echo "$loops" | tail -1)
-        tmp=$(mktemp -d)
-        mount "/dev/mapper/$part" "$tmp"
+    loops=$(kpartx -avs "$WORK_DIR/ubuntu-core.img"  | cut -d' ' -f 3)
+    part=$(echo "$loops" | tail -1)
+    tmp=$(mktemp -d)
+    mount "/dev/mapper/$part" "$tmp"
 
-        mkdir -p "$tmp/system-data/var/lib/cloud/seed/nocloud-net/"
-        cp "$WORK_DIR/user-data" "$tmp/system-data/var/lib/cloud/seed/nocloud-net/"
-        cp "$WORK_DIR/meta-data" "$tmp/system-data/var/lib/cloud/seed/nocloud-net/"
+    mkdir -p "$tmp/system-data/var/lib/cloud/seed/nocloud-net/"
+    cp "$WORK_DIR/user-data" "$tmp/system-data/var/lib/cloud/seed/nocloud-net/"
+    cp "$WORK_DIR/meta-data" "$tmp/system-data/var/lib/cloud/seed/nocloud-net/"
 
-        umount "$tmp"
-        kpartx -d "$WORK_DIR/ubuntu-core.img"
-    }
+    umount "$tmp"
+    kpartx -d "$WORK_DIR/ubuntu-core.img"
+}
+ 
+create_cloud_init_config_uc20(){
+    cat <<EOF > "$WORK_DIR/data.cfg"
+#cloud-config
+  ssh_pwauth: True
+  users:
+   - name: user1
+     sudo: ALL=(ALL) NOPASSWD:ALL
+     shell: /bin/bash
+  chpasswd:
+   list: |
+    user1:ubuntu
+   expire: False
+  datasource_list: [ "None"]
+  datasource:
+    None:
+     userdata_raw: |
+      #!/bin/bash
+      echo test
+EOF
 
-    systemd_create_and_start_unit() {
-        printf "[Unit]\nDescription=For testing purposes\n[Service]\nType=simple\nExecStart=%s\n" "$2" > "/run/systemd/system/$1.service"
-        if [ -n "${3:-}" ]; then
-            echo "Environment=$3" >> "/run/systemd/system/$1.service"
-        fi
-        systemctl daemon-reload
-        systemctl start "$1"
-    }
+    loop=$(kpartx -avs "$WORK_DIR/image/ubuntu-core.img" | sed -n 2p | awk '{print $3}')
+    tmp=$(mktemp -d)
 
-    get_qemu_for_nested_vm(){
-        case "$ARCHITECTURE" in
-        amd64)
-            command -v qemu-system-x86_64
-            ;;
-        i386)
-            command -v qemu-system-i386
-            ;;
-        *)
-            echo "unsupported architecture"
-            exit 1
-            ;;
-        esac
-    }
+    mount "/dev/mapper/$loop" "$tmp"
+    mkdir -p "$tmp/data/etc/cloud/cloud.cfg.d/"
+    cp -f "$WORK_DIR/data.cfg" "$tmp/data/etc/cloud/cloud.cfg.d/"
+    umount "$tmp"
+}
 
-    export PORT=8022
-    export WORK_DIR=/tmp/work-dir
-    export QEMU=$(get_qemu_for_nested_vm)
-
-    # create ubuntu-core image
-    mkdir -p "$WORK_DIR"
-
-    if [[ "$IMAGE_URL" == *.img.xz ]]; then
-        curl -L -o "$WORK_DIR/ubuntu-core.img.xz" "$IMAGE_URL"
-        unxz "$WORK_DIR/ubuntu-core.img.xz"
-    elif [[ "$IMAGE_URL" == *.img ]]; then
-        curl -L -o "$WORK_DIR/ubuntu-core.img" "$IMAGE_URL"
-    else
-        echo "Image extension not supported, exiting..."
-        exit 1
+systemd_create_and_start_unit() {
+    printf "[Unit]\nDescription=For testing purposes\n[Service]\nType=simple\nExecStart=%s\n" "$2" > "/run/systemd/system/$1.service"
+    if [ -n "${3:-}" ]; then
+        echo "Environment=$3" >> "/run/systemd/system/$1.service"
     fi
+    systemctl daemon-reload
+    systemctl start "$1"
+}
 
+get_qemu_for_nested_vm(){
+    case "$ARCHITECTURE" in
+    amd64)
+        command -v qemu-system-x86_64
+        ;;
+    i386)
+        command -v qemu-system-i386
+        ;;
+    *)
+        echo "unsupported architecture"
+        exit 1
+        ;;
+    esac
+}
+
+export PORT=8022
+export WORK_DIR=/tmp/work-dir
+export QEMU=$(get_qemu_for_nested_vm)
+
+# create ubuntu-core image
+mkdir -p "$WORK_DIR"
+
+if [[ "$IMAGE_URL" == *.img.xz ]]; then
+    curl -L -o "$WORK_DIR/ubuntu-core.img.xz" "$IMAGE_URL"
+    unxz "$WORK_DIR/ubuntu-core.img.xz"
+elif [[ "$IMAGE_URL" == *.img ]]; then
+    curl -L -o "$WORK_DIR/ubuntu-core.img" "$IMAGE_URL"
+else
+    echo "Image extension not supported, exiting..."
+    exit 1
+fi
+
+if test "$(lsb_release -cs)" = focal; then
+    snap install swtpm-mvo --beta
+    create_cloud_init_config_uc20
+    systemd_create_and_start_unit nested-vm "${QEMU} -m 4096 -nographic \
+        -machine ubuntu-q35,accel=kvm -global ICH9-LPC.disable_s3=1 \
+        -net nic,model=virtio -net user,hostfwd=tcp::$PORT-:22 \
+        -drive file=/usr/share/OVMF/OVMF_CODE.secboot.fd,if=pflash,format=raw,unit=0,readonly=on \
+        -drive file=/usr/share/OVMF/OVMF_VARS.ms.fd,if=pflash,format=raw,unit=1 \
+        -chardev socket,id=chrtpm,path=/var/snap/swtpm-mvo/current/swtpm-sock \
+        -tpmdev emulator,id=tpm0,chardev=chrtpm -device tpm-tis,tpmdev=tpm0 \
+        -drive file=$WORK_DIR/ubuntu-core.img,cache=none,format=raw,id=disk1,if=none \
+        -device virtio-blk-pci,drive=disk1,bootindex=1"         
+else
     create_cloud_init_config
-
     systemd_create_and_start_unit nested-vm "${QEMU} -m 2048 -nographic -snapshot \
         -net nic,model=virtio -net user,hostfwd=tcp::$PORT-:22 \
         -serial mon:stdio -machine accel=kvm \
         $WORK_DIR/ubuntu-core.img"
-
-    if wait_for_ssh; then
-        prepare_ssh
-    else
-        echo "ssh not established, exiting..."
-        journalctl -u nested-vm -n 150
-        exit 1
-    fi
-
-    echo "Wait for first boot to be done"
-    while ! execute_remote "snap changes" | grep -q -E "Done.*Initialize system state"; do
-        sleep 1
-    done
-
-    echo "VM Ready"
 fi
 
+if wait_for_ssh; then
+    prepare_ssh
+else
+    echo "ssh not established, exiting..."
+    journalctl -u nested-vm -n 150
+    exit 1
+fi
 
+echo "Wait for first boot to be done"
+while ! execute_remote "snap changes" | grep -q -E "Done.*Initialize system state"; do
+    sleep 1
+done
 
-
+echo "VM Ready"
