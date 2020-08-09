15e2d4dca4a7370b60270e4f838be1b4a445f120
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2020-08-09 13:24:53 -0300

    Adding new configuration for testing images

---
+++ services/env/snapd-edge-smoke-amd64-uc20.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export SNAP_UT=uc20
+export ARCH_UT=amd64
+export BOARD_UT=pc-amd64
+export TARGET_CHANNEL=edge
+export TRELLO_CONFIG=trello_card_uc20_edge.yaml
---
+++ services/env/snapd-edge-smoke-pi4-uc20.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export SNAP_UT=uc20
+export ARCH_UT=arm64
+export BOARD_UT=pi4
+export TARGET_CHANNEL=edge
+export TRELLO_CONFIG=trello_card_uc20_edge.yaml
