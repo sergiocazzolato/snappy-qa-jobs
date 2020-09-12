0f443b27c90247a75bbff1fb8432e82438c81f08
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2020-09-11 23:04:05 -0300

    Fix issues creating the root keys

--- scripts/utils/add_root_key.sh
+++ scripts/utils/add_root_key.sh
@@ -18,10 +18,10 @@ execute_remote() {
 }
 
 # Create certificates in case those are not stored
-if [ ! -f ~/.ssh/"$CERT_NAME" ] || [ ! -f ~/.ssh/"$CERT_NAME".pub ]; then
-	ssh-keygen -t rsa -N "$PASSPHRASE" -f ./"$CERT_NAME"
+if [ ! -f "$CERT_NAME" ] || [ ! -f "$CERT_NAME" ]; then
+	ssh-keygen -t rsa -N "$PASSPHRASE" -f "$CERT_NAME"
 fi
 
 execute_remote "sudo mkdir -p /root/.ssh"
 execute_remote "sudo chmod 700 /root/.ssh"
-cat ~/.ssh/"$CERT_NAME".pub | execute_remote "sudo tee -a /root/.ssh/authorized_keys > /dev/null"
+cat "$CERT_NAME" | execute_remote "sudo tee -a /root/.ssh/authorized_keys > /dev/null"
