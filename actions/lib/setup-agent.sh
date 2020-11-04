db61d92d09f0f17cca7741518edf2cb1f5a112f5
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2020-11-04 00:40:01 -0300

    New script to update all the sa.json and split the setup for the containers

--- actions/lib/setup-agent.sh
+++ actions/lib/setup-agent.sh
@@ -16,7 +16,7 @@ chmod u=rwx,g=rxs,o=rx /usr/local/bin/spread
 
 # Add gcloud configuration file that is readable only by the spread group
 mkdir -p /home/ubuntu/.config/gcloud
-mv /home/ubuntu/actions-runner/sa.json /home/ubuntu/.config/gcloud/application_default_credentials.json
+cp sa.json /home/ubuntu/.config/gcloud/application_default_credentials.json
 chown root.spread /home/ubuntu/.config/gcloud/application_default_credentials.json
 chmod u=,g=r,o= /home/ubuntu/.config/gcloud/application_default_credentials.json
 
