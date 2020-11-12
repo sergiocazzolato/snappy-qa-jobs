107b779ae8a6f7d87ea09060019bbe635e83b70b
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2020-11-12 19:23:08 -0300

    updated the monitor script to print the instances status

---
+++ actions/check-status.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# ssh to machine
+# ssh ubuntu@<IP> -o IdentitiesOnly=yes -i <KEY_PATH>
+
+# setup-machine
+# SPREAD_EXTERNAL_ADDRESS=<IP> SPREAD_EXTERNAL_KEY=<KEY_PATH> spread2 -debug external:ubuntu-18.04-64-key:tasks/setup-machine
+
+# setup-agent
+# SPREAD_EXTERNAL_ADDRESS=<IP> SPREAD_AGENT_ID=<ID> SPREAD_GITHUB_TOKEN=<TOKEN> SPREAD_EXTERNAL_KEY=<KEY_PATH> spread2 -debug external:ubuntu-18.04-64-key:tasks/setup-agent
+
+# check-agent-status
+# SPREAD_EXTERNAL_ADDRESS=<IP> SPREAD_EXTERNAL_KEY=<KEY_PATH> spread2 -debug external:ubuntu-18.04-64-key:tasks/check-agent-status
+set -ex
+
+. variables.sh
+
+instances=$(( $(jq ". | length" $AGENTS) - 1 ))
+
+for key in $(seq "$instances"); do
+    jq ".[$key].name" agents.json
+    ip=$(jq -r ".[$key].ip" $AGENTS)
+
+    ssh -o IdentitiesOnly=yes -i $SPREAD_EXTERNAL_KEY $USER@$ip lxd.lxc list
+    SPREAD_EXTERNAL_ADDRESS=$ip $SPREAD -debug external:"$SYSTEM":tasks/check-agent-status
+done
+
