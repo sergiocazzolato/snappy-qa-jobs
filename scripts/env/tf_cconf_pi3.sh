207a384c535779029556636eac0769c9f9a028b9
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2020-11-16 17:01:27 -0300

    update queue for rpi3

--- scripts/env/tf_cconf_pi3.sh
+++ scripts/env/tf_cconf_pi3.sh
@@ -5,7 +5,7 @@
 export ARCH=${ARCH:-"armhf"}
 export PROJECT=${PROJECT:-"console-conf-tests"}
 export CHANNEL=${CHANNEL:-"beta"}
-export DEVICE_QUEUE=${DEVICE_QUEUE:-"rpi3"}
+export DEVICE_QUEUE=${DEVICE_QUEUE:-"rpi3b"}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-arm-32"}
 export SPREAD_PARAMS=${SPREAD_PARAMS:-"-v"}
 export SPREAD_ENV=${SPREAD_ENV:-"WIFI_READY=false"}
