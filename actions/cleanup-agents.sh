b3244e4177482fcf70cda0410d3e34c2c12771f1
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-03-05 17:04:03 -0300

    New cleanup scripts

---
+++ actions/cleanup-agents.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -ex
+
+if hostname | grep prod-snapd-testing; then
+    . variables-prodstack.sh
+else
+    . variables-canonistack.sh
+fi
+
+instances=$(jq ". | length" $AGENTS)
+
+for key in $(seq "$instances"); do
+    echo "-------------------------------------------"
+    iter=$(( $key - 1 ))
+    jq ".[$iter].name" "$AGENTS"
+    ip=$(jq -r ".[$iter].ip" $AGENTS)
+
+    echo "Disk before cleanup"
+    ssh -o IdentitiesOnly=yes -i $SPREAD_EXTERNAL_KEY $USER@$ip sudo lxd.lxc storage info default
+
+    SPREAD_EXTERNAL_ADDRESS=$ip "$SPREAD" external:"$SYSTEM":tasks/cleanup-agents
+
+    echo "Disk after cleanup"
+    ssh -o IdentitiesOnly=yes -i $SPREAD_EXTERNAL_KEY $USER@$ip sudo lxd.lxc storage info default
+done
+
