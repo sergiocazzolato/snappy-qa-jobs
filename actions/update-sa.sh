b5b4ae18679763b4339f2d9a69a55762d957077d
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-04-29 13:20:59 -0300

    Make posible to update the sa.json file by project
    All the projects use their sa.json file.

--- actions/update-sa.sh
+++ actions/update-sa.sh
@@ -7,6 +7,17 @@ else
     . variables-canonistack.sh
 fi
 
+target_project=${1:-}
+sa_file=${2:-}
+if [ -z "$target_project" ]; then
+	echo "project missing as parameter, exiting..."
+	exit
+fi
+if [ -z "$sa_file" ]; then
+	echo "service acount file missing as parameter, exiting..."
+	exit
+fi
+
 instances=$(jq ". | length" $AGENTS)
 
 for key in $(seq "$instances"); do
@@ -14,8 +25,13 @@ for key in $(seq "$instances"); do
     iter=$(( $key - 1 ))
     jq ".[$iter].name" "$AGENTS"
     ip=$(jq -r ".[$iter].ip" $AGENTS)
+    curr_project=$(jq -r ".[$iter].project" $AGENTS)
 
-    ssh -o IdentitiesOnly=yes -i $SPREAD_EXTERNAL_KEY $USER@$ip sudo lxd.lxc list
-    SPREAD_EXTERNAL_ADDRESS=$ip "$SPREAD" external:"$SYSTEM":tasks/update-service-account
+    if [ "$target_project" = "$curr_project" ]; then
+    	ssh -o IdentitiesOnly=yes -i $SPREAD_EXTERNAL_KEY $USER@$ip sudo lxd.lxc list
+    	SPREAD_EXTERNAL_ADDRESS=$ip SPREAD_SA_FILE="$sa_file" "$SPREAD" external:"$SYSTEM":tasks/update-service-account
+    else
+    	echo "Skipping service account configuration for project $curr_project"
+    fi
 done
 
